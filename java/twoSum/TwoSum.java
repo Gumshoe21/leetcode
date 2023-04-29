ceba03762dff5ee579e07093add507d586aa1e18
Matthew Smilansky <msmilansky@gmail.com>
2023-04-28 19:40:25 -0700

    Improve TwoSum.java functionality so as to permit command-line args to fn params.

--- java/twoSum/TwoSum.java
+++ java/twoSum/TwoSum.java
@@ -46,9 +46,14 @@ public class TwoSum {
   }
 
   public static void main(String[] args) {
-    int[] nums = {2, 7, 11, 15};
-    int target = 9;
-    int[] result = twoSum(nums, target);
+    String[] splitArgs = args[0].split(",");
+    int[] intArray = new int[splitArgs.length];
+
+    for (int i = 0; i < intArray.length; i++) {
+      intArray[i] = Integer.parseInt(splitArgs[i]);
+    }
+
+    int[] result = twoSum(intArray, Integer.parseInt(args[1]));
 
     System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
   }
