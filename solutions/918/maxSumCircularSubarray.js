dc6e1086a0013067b113e997af778bb2141d6294
Matthew Smilansky <msmilansky@gmail.com>
2023-12-15 22:44:05 -0800

    Add 1436

--- solutions/918/maxSumCircularSubarray.js
+++ solutions/918/maxSumCircularSubarray.js
@@ -17,3 +17,24 @@ var maxSubarraySumCircular = function (nums) {
 
 	return globalMax > 0 ? Math.max(globalMax, total - globalMin) : globalMax;
 };
+
+/**
+ * 
+ * We want to find the following:
+ * 	- The sum of the array within nums that has the smallest sum (globMin)
+ *  - The sum array within nums that has the largest sum (globMax)
+ *	- The total sum of nums 
+ *	
+ * With these three values, we can easily find out which out whether the greatest array (including circular arrays) is either part of the original array whole (i.e. not circular), or part of a circular array.
+ * 
+ * By subtracting the globMin from the total 
+ */
+/*
+[20, -2, 5, -2, 20]
+41 
+
+currMin - 20  -2  3   -2  17
+currMax - 20  18  23  20  40
+globMin - 20 -2 -2    -2  -2 
+globMax - 20 20 23 23  40
+*/
